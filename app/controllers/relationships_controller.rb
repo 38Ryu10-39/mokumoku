b9ff5289dd0cbb48f2f6c57c82adeee7e4cc7b2c
38Ryu10-39 <keshi-.-tp_38@docomo.ne.jp>
2023-08-16 20:43:30 +0900

    add: Follow/UnFollow機能の追加、Usersテーブルにprofileとhobbyの追加

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,13 @@
+class RelationshipsController < ApplicationController
+	# フォローする
+  def create
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
+  end
+  
+  # フォロー外す
+  def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer  
+  end
+end
